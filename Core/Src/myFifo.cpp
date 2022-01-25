17a3309ba972889412aaacf440ef2a050957d63d
John Vishnefske <jvishnefske@gmail.com>
2022-01-24 23:06:58 -0600

    added fifo

---
+++ Core/Src/myFifo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by John Vishnefske on 1/11/22.
+//
+
+#include "myFifo.h"
---
+++ Core/Src/myFifo.h
@@ -0,0 +1,16 @@
+//
+// Created by John Vishnefske on 1/11/22.
+//
+
+#ifndef MCU2_MYFIFO_H
+#define MCU2_MYFIFO_H
+
+
+
+class myFifo {
+
+};
+
+
+
+#endif //MCU2_MYFIFO_H
